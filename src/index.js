2e51b284280141d5d862abcbca7c68a79b9d9e68
Alex Bukhman <alexb823@gmail.com>
2019-03-06 21:58:06 -0500

    finished

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
-const People = () => <div>People</div>;
+const People = ({ people, location, match }) => {
+  if (match.params.id) {
+    console.log(`do something with ${match.params.id}`);
+  }
+  return (
+    <ul>
+      {people.map(person => {
+        return (
+          <li key={person.id}>
+            <Link to={`/people/${person.id}`}>{person.name}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 const Places = () => <div>Places</div>;
 const Things = () => <div>Things</div>;
 
 const Nav = ({ location }) => {
   const pathname = location.pathname;
   const links = ['/people', '/places', '/things'];
-
   return (
     <ul className="nav nav-tabs">
       {links.map(link => (
         <li key={link} className="nav-item">
           <Link
             to={link}
-            className={`nav-link ${link === pathname ? 'active' : ''}`}
+            className={`nav-link ${pathname.startsWith(link) ? 'active' : ''}`}
           >
             {link.slice(1)}
           </Link>
@@ -27,14 +47,35 @@ const Nav = ({ location }) => {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      people: [
+        { id: 1, name: 'moe' },
+        { id: 2, name: 'larry' },
+        { id: 3, name: 'curly' },
+      ],
+    };
+  }
   render() {
+    const { people } = this.state;
+    console.log(people);
     return (
       <Router>
         <div>
           <Route component={Nav} />
-          <Route path="/people" component={People} />
-          <Route path="/places" component={Places} />
-          <Route path="/things" component={Things} />
+          <Switch>
+            <Route
+              exact
+              path="/people/:id?"
+              render={({ location, match }) => (
+                <People people={people} location={location} match={match} />
+              )}
+            />
+            <Route path="/places" component={Places} />
+            <Route path="/things" component={Things} />
+            <Redirect to="/people" />
+          </Switch>
         </div>
       </Router>
     );
